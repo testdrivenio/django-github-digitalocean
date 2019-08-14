1253fcd3763855bf083e54ff5a5ec25ebab5a338
Michael Herman <hermanmu@gmail.com>
2019-08-14 07:31:51 -0600

    init

---
+++ app/hello_django/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.http import JsonResponse
+
+
+def home(request):
+    return JsonResponse({"hello": "world"})
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('admin/', admin.site.urls),
+]
+
+if bool(settings.DEBUG):
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
