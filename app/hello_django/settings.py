ba4365096a3a52431a354d2eb2fdcf0b441e6b24
Gary McNeish <garymcneish1987@gmail.com>
2021-01-09 17:56:17 +0000

    added all hosts

--- app/hello_django/settings.py
+++ app/hello_django/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '138.68.7.194' '178.62.84.37']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '138.68.7.194' '178.62.84.37' '*']
 
 # Application definition
 
