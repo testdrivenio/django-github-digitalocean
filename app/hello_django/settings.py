432c5d16c689623c94bf6ccd9ebab53eff243426
Michael Herman <hermanmu@gmail.com>
2021-06-26 22:18:11 -0500

    testing github actions

--- app/hello_django/settings.py
+++ app/hello_django/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '165.232.151.68']
 
 # Application definition
 
